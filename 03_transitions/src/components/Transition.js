f6ab6b79fd3ab49782daca6ac91da88f56a2a5a6
Rute <rute.s.abreu@gmail.com>
2019-03-20 17:33:07 -0300

    React transitions

---
+++ 03_transitions/src/components/Transition.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition'
+
+import '../css/App.css';
+
+class TransitionComp extends Component{
+
+
+    state = {
+        show:true
+    }
+
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show ? true:false
+        })
+    }
+    render(){
+        return(
+            <div>
+                <Transition
+                    in = {this.state.show}
+                    timeout = {{
+                        enter:2000,
+                        exit:50
+                    }}
+
+                    enter = {true}
+                    false = {true}
+
+                    onEnter = {(node) => {
+                        console.log('Enter')
+
+                    }}
+
+                    onExit = {(node ) => {
+                        console.log("Exit")
+                    }}
+                >
+                    { state =>
+                        <div className = {`square square-${state}`}>
+                            { `square square-${state}` }
+                        </div>
+                    }
+                </Transition>
+                
+                <div className = "showDiv"
+                    onClick = {this.showDiv}>
+                    Show or Hide
+                </div>
+            </div>
+
+
+
+        )
+    }
+}
+
+
+export default TransitionComp;
